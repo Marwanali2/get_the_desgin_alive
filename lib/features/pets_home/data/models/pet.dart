2eb4597f88fff0f2d4944cdd2f673f7e73bac123
marwan ali <marwanalizz42@gmail.com>
2023-07-20 01:14:11 +0300

    add dummy data

---
+++ lib/features/pets_home/data/models/pet.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Pet {
+  final String animal;
+  final String type;
+  final num age;
+  final String photo;
+
+  Pet(this.animal, this.type, this.age, this.photo);
+
+
+}
